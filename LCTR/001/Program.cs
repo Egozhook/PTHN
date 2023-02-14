4bde5c99a321779bb133de107b67f53e36015701
egozhook <egozhook@gmail.com>
2023-02-14 11:08:32 +0400

    Commit002

--- LCTR/001/Program.cs
+++ LCTR/001/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿//Напишите программу, которая на вход принимает число (N > 0), а на выходе показывает все
+//чётные числа от 1 до N.
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (num > count)
+{
+    Console.Write(count + ", ");
+    count=count+2;
+}
