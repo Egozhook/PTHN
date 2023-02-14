bd8a29904f88389c5acda7211f399ab7b322a6c8
egozhook <egozhook@gmail.com>
2023-02-14 12:06:16 +0400

    HMWRK003

--- HMWRK/003/Program.cs
+++ HMWRK/003/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("22154Hello, World!");
+﻿void Zadacha001()
+{   //Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+    Console.WriteLine("Введите пятизначное число: ");
+    int digit = Convert.ToInt32(Console.ReadLine());
+    if (digit > 9999 && digit < 100000)
+    {
+        if (digit/10000 == digit%10)
+        {
+            if (digit/1000%10 == digit/10%10)
+            {
+                Console.WriteLine($"Число {digit} - палиндром");
+            }
+            else Console.WriteLine($"Число {digit} - не является палиндромом");
+
+        }
+        else Console.WriteLine($"Число {digit} - не является палиндромом");
+    }
+    else Console.WriteLine("Неверный ввод");
+}
+void Zadacha002()
+{   //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+    Console.WriteLine("Введите координаты точки А: x = :");
+    int x_first = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите координаты точки А: у = :");
+    int y_first = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите координаты точки А: z = :");
+    int z_first = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите координаты точки Б: х = :");
+    int x_second = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите координаты точки А: у = :");
+    int y_second = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите координаты точки А: z = :");
+    int z_second = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Расстояние между точками: {Math.Sqrt(Math.Pow(x_second - x_first, 2) + Math.Pow(y_second - y_first, 2) + Math.Pow(z_second - z_first, 2))}");
+}
+void Zadacha003()
+{   //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+    Console.WriteLine("Введите число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Число\tКуб");
+    for (int i = 1; i <= number; i++)
+    {
+        Console.WriteLine("{0}\t{1}\t", i, Math.Pow(i, 3));
+    }
+
+}
+//Zadacha001();
+//Zadacha002();
+//Zadacha003();
