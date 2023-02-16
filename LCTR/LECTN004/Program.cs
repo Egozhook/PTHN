cf5ab2fd8693bfd10ef9fb4c633f6e1fd4ffc999
egozhook <egozhook@gmail.com>
2023-02-16 12:31:05 +0400

    LCTR004

---
+++ LCTR/LECTN004/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+int a = 12;
+Console.WriteLine(a.GetType().Name);
+
+var data = new int[] {1,2,3,4,5}
+            .Where( e => e >0)
+            .Select(e => new { q=e, w = e =1});
+Console.WriteLine(data.GetType().Name);
